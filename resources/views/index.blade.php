788368b13a0f115a6e4682a61e59325744dd31a0
muh-ilhamkurniawan <93339557+muh-ilhamkurniawan@users.noreply.github.com>
2024-07-09 08:27:07 +0700

    Add Dashboard Template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,47 +34,193 @@
         <div class="container-fluid">
             <div class="row justify-content-center">
                 <div class="col-12">
-                    <h2 class="mb-2 page-title">Data Awal</h2>
-                    {{-- <p class="card-text">Lihat Data Kategori</p>
-                    @if (session('success'))
-                    <div class="alert alert-success">
-                        {{ session('success') }}
-                    </div>
-                    @endif
-                    <div class="row my-4">
-                        <!-- Small table -->
-                        <div class="col-md-12">
-                            <div class="card shadow">
-                                <div class="card-body">
-                                    <!-- table -->
-                                    <table class="table datatables" id="dataTable-1">
-                                        <thead>
-                                            <tr>
-                                                <th>Kode Kategori</th>
-                                                <th>Nama Kategori</th>
-                                                <th>Aksi</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ($kelass as $kelas)
-                                            <tr>
-                                                <td>{{ $kelas->kode_kelas }}</td>
-                                                <td>{{ $kelas->nama_kelas }}</td>
-                                                <td>
-                                                    <form action="{{ route('kelas.destroy', $kelas->kode_kelas) }}" method="POST">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus kelas ini?')">Hapus</button>
-                                                    </form>
-                                                </td>
-                                            </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
+                    <h2 class="mb-2 page-title">Dashboard</h2>
+                    <p class="card-text">Lihat Data Kategori</p>
+                    <div class="row">
+                      <div class="col-md-6 col-xl-3 mb-4">
+                          <div class="card shadow">
+                            <div class="card-body">
+                              <div class="row align-items-center">
+                                <div class="col-3 text-center">
+                                  <span class="circle circle-sm bg-primary">
+                                    <i class="fe fe-16 fe-box text-white mb-0"></i>
+                                  </span>
                                 </div>
+                                <div class="col pr-0">
+                                  <p class="small text-muted mb-0">Total Kereta</p>
+                                  <span class="h3 mb-0">
+                                  <?php
+                                    // $resultcount = $conn->query("select count(no_ka) as total from kereta");
+                                    // $totalcount = $resultcount->fetch_assoc();
+                                    // echo $totalcount['total'];
+                                    ?>
+                                  </span>
+                                </div>
+                              </div>
                             </div>
-                        </div> <!-- simple table -->
-                    </div> <!-- end section --> --}}
+                          </div>
+                        </div>
+                        <div class="col-md-6 col-xl-3 mb-4">
+                          <div class="card shadow">
+                            <div class="card-body">
+                              <div class="row align-items-center">
+                                <div class="col-3 text-center">
+                                  <span class="circle circle-sm bg-primary">
+                                    <i class="fe fe-16 fe-grid text-white mb-0"></i>
+                                  </span>
+                                </div>
+                                <div class="col pr-0">
+                                  <p class="small text-muted mb-0">Kategori</p>
+                                  <span class="h3 mb-0">
+                                  <?php
+                                    // $resultcount = $conn->query("select count(kode_kelas) as total from kelas");
+                                    // $totalcount = $resultcount->fetch_assoc();
+                                    // echo $totalcount['total'];
+                                    ?>
+                                  </span>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-md-6 col-xl-3 mb-4">
+                          <div class="card shadow">
+                            <div class="card-body">
+                              <div class="row align-items-center">
+                                <div class="col-3 text-center">
+                                  <span class="circle circle-sm bg-primary">
+                                    <i class="fe fe-16 fe-pie-chart text-white mb-0"></i>
+                                  </span>
+                                </div>
+                                <div class="col">
+                                  <p class="small text-muted mb-0">Keterlambatan</p>
+                                  <div class="row align-items-center no-gutters">
+                                    <div class="col-auto">
+                                      <span class="h3 mr-2 mb-0">
+                                      <?php
+                                        // $resultcount = $conn->query("select count(no_ka) as total from transaksitelat");
+                                        // $totalcount = $resultcount->fetch_assoc();
+                                        // echo $totalcount['total'];
+                                        ?>
+                                      </span>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <?php
+                        // if ($_SESSION['b_level']=='admin') {
+                          ?>
+                        <div class="col-md-6 col-xl-3 mb-4">
+                          <div class="card shadow">
+                            <div class="card-body">
+                              <div class="row align-items-center">
+                                <div class="col-3 text-center">
+                                  <span class="circle circle-sm bg-primary">
+                                    <i class="fe fe-16 fe-user text-white mb-0"></i>
+                                  </span>
+                                </div>
+                                <div class="col">
+                                  <p class="small text-muted mb-0">Pengguna</p>
+                                  <span class="h3 mb-0">
+                                  <?php
+                                    // $resultcount = $conn->query("select count(id_admin) as total from admin");
+                                    // $totalcount = $resultcount->fetch_assoc();
+                                    // echo $totalcount['total'];
+                                    ?>
+                                  </span>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <?php 
+                          // }
+                        ?>
+                      </div> <!-- end section -->
+                      <!-- info small box -->
+                      <div class="row">
+                        <!-- Recent orders -->
+                        <div class="col-md-12">
+                          <h6 class="mb-3">Data Keterlambatan Terbaru</h6>
+                          <table class="table table-borderless table-striped">
+                            <thead>
+                              <tr role="row">
+                                <th>ID</th>
+                                <th>Tanggal</th>
+                                <th>Nama Kereta</th>
+                                <th>Keterlambatan Datang</th>
+                                <th>Keterlambatan Berangkat</th>
+                                <th>Keterangan</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                                    // $sqldetail = "select distinct transaksitelat.no_ka,transaksitelat.id, kereta.nama,kereta.kelas, kereta.kedatangan, kereta.keberangkatan,transaksitelat.tanggal,transaksitelat.lama_datang,transaksitelat.lama_berangkat,transaksitelat.kedatangan as real_datang, transaksitelat.keberangkatan as real_berangkat, transaksitelat.alasan from transaksitelat join kereta on transaksitelat.no_ka = kereta.no_ka order by transaksitelat.id desc limit 5";
+                                    // $resultDetail = $conn->query($sqldetail);
+                                    // if ($resultDetail->num_rows>0) {
+                                    //     while ($rowDetail=$resultDetail->fetch_assoc()) {
+                                            ?>
+                                            <tr>
+
+                                            </tr>
+                                            <?php
+                                    //     }
+                                    // }
+                                    ?>
+                            </tbody>
+                          </table>
+                        </div> <!-- / .col-md-3 -->
+                      </div> <!-- end section -->
+                      <h6 class="mb-3">Informasi Aplikasi</h6>
+                      <p class="text-muted"></p>
+                      <div class="row">
+                        <div class="col-md-12 mb-4">
+                          <div class="card profile shadow">
+                            <div class="card-body my-4">
+                              <div class="row align-items-center">
+                                <div class="col-md-3 text-center mb-5">
+                                  <a href="#!" class="avatar avatar-xl">
+                                    <img src="{{ asset('images/kai.png') }}" alt="..." class="avatar-img rounded-circle">
+                                  </a>
+                                </div>
+                                <div class="col">
+                                  <div class="row align-items-center">
+                                    <div class="col-md-7">
+                                      <h4 class="mb-1">Tentang SIMAKER</h4>
+                                      <p class="small mb-3"><span class="badge badge-dark">Sistem Informasi Manajemen Keterlambatan Kereta</span></p>
+                                    </div>
+                                    <div class="col">
+                                    </div>
+                                  </div>
+                                  <div class="row mb-4">
+                                    <div class="col-md-7">
+                                      <p class="text-muted">Sistem Informasi Manajamen Keterlambatan Kereta Api merupakan sebuah sistem informasi berbasis website yang berfungsi untuk mencatat, mengedit, serta melihat laporan keterlambatan kereta api di wilayah Daop 5 Purwokerto, khususnya pada stasiun Kutoarjo. </p>
+                                    </div>
+                                    <div class="col">
+                                      <p class="small mb-0 text-muted">PT Kereta Api Indonesia(Persero)</p>
+                                      <p class="small mb-0 text-muted">Daop 5 Purwokerto</p>
+                                      <p class="small mb-0 text-muted">(0281) 636031</p>
+                                    </div>
+                                  </div>
+                                  <div class="row align-items-center">
+                                    <div class="col-md-7 mb-2">
+                                      <span class="small text-muted mb-0">Dibuat sebagai hasil kerja praktek </br>Mahasiswa Informatika Universitas Jendral Soedirman </br> @2023</span>
+                                    </div>
+                                    <div class="col mb-2">
+                                      <a href="https://drive.google.com/drive/folders/1tPgN_U5jQ-GrAbes7q9glwA6_E55qiLZ?usp=sharing" target="_blank">
+                                      <button type="button" class="btn btn-primary">Unduh Panduan</button>
+                                      </a>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div> <!-- / .row- -->
+                            </div> <!-- / .card-body - -->
+                          </div> <!-- / .card- -->
+                        </div> <!-- / .col- -->
+                      </div> <!-- end section -->
                 </div> <!-- .col-12 -->
             </div> <!-- .row -->
         </div> <!-- .container-fluid -->
